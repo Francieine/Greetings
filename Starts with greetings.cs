be2d623bde7c6bef490eb9614cd628aa29752c24
Francieine <73969123+Francieine@users.noreply.github.com>
2022-02-28 17:32:06 -0300

    Add files via upload
    How can you starts your project? This is a simple exemple. Just try it!

---
+++ Starts with greetings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You can starts your programs with greetings//
+
+            Console.WriteLine("Hello and Welcome! Explore and enjoy this!");
+
+            Console.ReadLine();
+        }
+    }
+}
